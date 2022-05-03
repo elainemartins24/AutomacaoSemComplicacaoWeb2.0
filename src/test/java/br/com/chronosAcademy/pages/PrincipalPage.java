ed47312c1a56da6a15a5531a32f3df4b90a09454
elainemartins24 <emrso@uol.com.br>
2022-05-02 21:43:50 -0300

    aula 10 Page Factory

--- src/test/java/br/com/chronosAcademy/pages/PrincipalPage.java
+++ src/test/java/br/com/chronosAcademy/pages/PrincipalPage.java
@@ -1,26 +1,37 @@
 package br.com.chronosAcademy.pages;
 
+import br.com.chronosAcademy.maps.PrincipalMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class PrincipalPage {
+    //classe criada especificamente para as ações
     WebDriver driver;
+    PrincipalMap principalMap;  // para poder utilizar os elementos definidos na classe PrincipalMap
+
 
     public PrincipalPage(WebDriver driver) {  //Cria construtor que vai receber o drive
+
         this.driver = driver;
+        principalMap = new PrincipalMap(); // para instanciar
+        PageFactory.initElements(driver, principalMap); // inicializa os objetos que estão na classe principalMap
     }
+
     public String getTitulo() {
-        String xpathTitulo = "//section[2]//h4"; // cria o objeto xpathTitulo com o caminho do título da página
-
-        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo)); //Objeto que localiza o elemento
-        String titulo = txtTitulo.getText(); //Objeto que recebe o valor vindo do elemento
-        return titulo;
+        return principalMap.txtTitulo.getText(); //Objeto que recebe o valor vindo do elemento
     }
-    public void clickBotao() {
-        String xpathBotao = "//section[2]/div[3]//a";
 
-        WebElement btnTitulo = driver.findElement(By.xpath(xpathBotao)); //Localiza o botão
-        btnTitulo.click();  // Clica no botão
+    public void clickBotao() {
+        principalMap.btnTitulo.click();  // Clica no botão
+    }
+
+    public PrincipalMap getPrincipalMap() {
+        return principalMap;
+    }
+
+    public void setPrincipalMap(PrincipalMap principalMap) {
+        this.principalMap = principalMap;
     }
 }
